0dad94b40ecc5b1f1bd207b8c6fa7fdfb6abdf45
Yupeng Wang <wypgreat@icloud.com>
2026-04-25 22:27:51 +0100

    hello-wrold chain

--- main.py
+++ main.py
@@ -1,8 +1,38 @@
-import os
 from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_ollama import ChatOllama
+from langchain_openai import ChatOpenAI
+
 load_dotenv()
+
+
 def main():
     print("Hello from langchain-course!")
+
+    # Information get the text input from user, it could be reusable in langchain
+    information = """
+    Elon Reeve Musk (/ˈiːlɒn/ EE-lon; born June 28, 1971) is a businessman and entrepreneur known for his leadership of Tesla, SpaceX, X, and xAI. 
+    Musk has been the wealthiest person in the world since 2025; as of April 2026, Forbes estimates his net worth to be US$809 billion.
+    """
+
+    # This is the template that will send to Chat model
+    summary_template = """
+        given the information {information} about a person I want you to create:
+        1. A short summary
+        2. two interesting facts about them
+    """
+
+    summary_prompt_template = PromptTemplate(
+        input_variables=["information"], template=summary_template
+    )
+
+    # Create llm chat model
+    # llm = ChatOllama(temperature=0, model="gemma3:270m")
+    llm = ChatOpenAI(temperature=0, model="gpt-5")
+    chain = summary_prompt_template | llm
+    response = chain.invoke(input={"information": information})
+    print(response.content)
+
 
 if __name__ == "__main__":
     main()
